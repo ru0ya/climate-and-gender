78017f96f90dacb8725394be8ec328a8d6fdeaab
01Laurent <kiirulaurent@gmail.com>
2024-10-04 21:59:43 +0300

    login

--- nasa/genderclimate/urls.py
+++ nasa/genderclimate/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from .views import HomeView, user_profile_view
+from .views import HomeView, user_register_view
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', HomeView, name='home'),
-    path('register/', user_profile_view, name='register'),
+    path('register/', user_register_view, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-
 ]
--- nasa/genderclimate/views.py
+++ nasa/genderclimate/views.py
@@ -1,39 +1,19 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
-from .models import UserProfile
 from django.contrib.auth.decorators import login_required
 from .forms import UserProfileForm
 
 def HomeView(request):
     return render(request, 'home.html')
 
-@login_required
-def user_profile_view(request):
-    # Check if the user already has a profile
-    try:
-        profile = UserProfile.objects.get(user=request.user)
-    except UserProfile.DoesNotExist:
-        profile = None
 
+def user_register_view(request):
     if request.method == 'POST':
-        if profile:
-            # If the user has a profile, update it
-            form = UserProfileForm(request.POST, instance=profile)
-        else:
-            # If the user doesn't have a profile, create a new one
-            form = UserProfileForm(request.POST)
-        
+        form =UserProfileForm(request.POST)
         if form.is_valid():
-            user_profile = form.save(commit=False)
-            user_profile.user = request.user  # Assign the user to the profile
-            user_profile.save()
-            # return redirect('profile_success')  # Redirect to a success page or the user dashboard
+            form.save()  #save both User and UserProfile data
+            return redirect('login')
     else:
-        if profile:
-            # Populate the form with the existing profile information
-            form = UserProfileForm(instance=profile)
-        else:
-            # Empty form for creating a new profile
-            form = UserProfileForm()
+        form = UserProfileForm()
 
     return render(request, 'register.html', {'form': form})
