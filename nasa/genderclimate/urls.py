5897a9c4b6b0bfcbd947fb5c88814f70a3edce02
01Laurent <kiirulaurent@gmail.com>
2024-10-04 23:34:26 +0300

    logout

--- nasa/genderclimate/urls.py
+++ nasa/genderclimate/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', HomeView, name='home'),
     path('register/', user_register_view, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout')
 ]
