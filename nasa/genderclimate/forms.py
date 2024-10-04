90e804cee4b187c6d1e378a18a435502185afdc1
01Laurent <kiirulaurent@gmail.com>
2024-10-04 21:23:00 +0300

    login

--- nasa/genderclimate/forms.py
+++ nasa/genderclimate/forms.py
@@ -1,28 +1,35 @@
 from django import forms
+from django.contrib.auth.models import User
 from  .models import UserProfile
+from django.contrib.auth.forms import UserCreationForm
 
 
-class UserProfileForm(forms.ModelForm):
-    class Meta:
-        model = UserProfile
-        fields = ['age', 'gender', 'location', 'health_condition', 'email', 'phone', 'next_of_kin']
+
+class UserProfileForm(UserCreationForm):
+    age = forms.IntegerField(required=False, widget=forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Enter your age'}))
+    gender = forms.ChoiceField(choices=UserProfile.GENDER_CHOICES, widget=forms.Select(attrs={'class': 'form-control'}))
+    location = forms.CharField(required=False, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter your location'}))
+    health_condition = forms.ChoiceField(choices=UserProfile.HEALTH_CONDITION_CHOICES, widget=forms.Select(attrs={'class': 'form-control'}))
+    phone = forms.CharField(required=False, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter your phone number'}))
+    next_of_kin = forms.CharField(required=False, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Next of kin phone number'}))
+
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2', 'age', 'gender', 'location', 'health_condition', 'email', 'phone', 'next_of_kin']
+
+    def save(self, commit=True):
+        user = super().save(commit=False)  # Save the User fields first
         
-        widgets = {
-            'age': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Enter your age'}),
-            'gender': forms.Select(attrs={'class': 'form-control'}),
-            'location': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter your location'}),
-            'health_condition': forms.Select(attrs={'class': 'form-control'}),
-            'email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Enter your email'}),
-            'phone': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter your phone number'}),
-            'next_of_kin': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Next of kin phone number'}),
-        }
+        if commit:
+            user.save()
+            UserProfile.objects.create(
+                user=user,
+                age=self.cleaned_data['age'],
+                gender=self.cleaned_data['gender'],
+                location=self.cleaned_data['location'],
+                health_condition=self.cleaned_data['health_condition'],
+                phone=self.cleaned_data['phone'],
+                next_of_kin=self.cleaned_data['next_of_kin']
+            )
         
-        labels = {
-            'age': 'Age',
-            'gender': 'Gender',
-            'location': 'Location',
-            'health_condition': 'Health Condition',
-            'email': 'Email',
-            'phone': 'Phone Number',
-            'next_of_kin': 'Next of Kin',
-        }
+        return user
--- nasa/genderclimate/urls.py
+++ nasa/genderclimate/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from .views import HomeView, user_profile_view
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', HomeView, name='home'),
-    path('register', user_profile_view, name='register'),
+    path('register/', user_profile_view, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+
 ]
